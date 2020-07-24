62e2a5869a32007855752280c3e8b7d536b3c9e5
strashevskaya <krisssshik@gmail.com>
2020-07-24 12:31:10 +0300

    changed config.yml file

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
+import utils.CapabilitiesGenerator;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,21 +16,18 @@ public class BaseTest {
     NewProjectPage newProjectPage;
     ProjectDetailsPage projectDetailsPage;
     BillingPage billingPage;
-    ChatPage chatPage;
     WebDriver driver;
 
 
     @BeforeMethod
     public void setDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         loginPage = new LoginPage(driver);
         projectsPage = new ProjectsPage(driver);
         newProjectPage = new NewProjectPage(driver);
         projectDetailsPage = new ProjectDetailsPage(driver);
-        chatPage = new ChatPage(driver);
         billingPage = new BillingPage(driver);
 
     }
